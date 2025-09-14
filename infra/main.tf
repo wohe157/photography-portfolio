5b00836176b3aea00c0ac8c8b86f73fc2943d28e
Wouter Heyvaert <wouterheyvaert1@gmail.com>
2025-09-14 18:06:27 +0200

    Store terraform state in S3

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~>6.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "wh-photography-portfolio-terraform-state"
+    key          = "terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
 provider "aws" {
